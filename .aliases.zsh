6d414e2205b835653e0ca770cb0f16c159eefea8
Dylan Carney <dcarney@gmail.com>
2014-11-17 17:27:45 -0800

    Cleans up aliases and location shortcuts

--- .aliases.zsh
+++ .aliases.zsh
@@ -1,6 +1,17 @@
+# essentials
 alias ll='ls -alh'
 alias lll='ls -lasht'
 alias c='clear'
+alias ..='cd ..'
+alias hist='history'
+alias h='history'
+alias hgrep='history | grep'
+alias untar='tar xvfz'
+alias xx='exit'
+alias please='sudo'
+alias fuck='sudo $(fc -ln -1)'
+
+# git related
 alias g='git'
 alias gs='c; git s'
 alias gss='c; git ss'
@@ -12,22 +23,21 @@ alias gr='git checkout --'
 alias gpr='git pr'
 alias gpom='git pull origin master'
 alias gprom='git pull --rebase origin master'
-alias ..='cd ..'
+
+# locations
 alias src='cd ~/src'
 alias api='cd ~/src/relevancy_api'
 alias backend='cd ~/src/relevancy_backend'
-alias conf='cd ~/src/relevancy_conf'
+alias conf='cd ~/src/core_conf'
 alias lib='cd ~/src/java_libraries'
-alias hist='history'
-alias h='history'
-alias hgrep='history | grep'
-alias untar='tar xvfz'
+alias misc='cd ~/misc'
+alias golang="cd $GOPATH"
+
+# misc
 alias unjar='jar xvf'
 alias fs="stat -f \"%z bytes\"" # File size
-alias please='sudo'
 alias head='head -n 10'
 alias tail='tail -n 10'
-alias xx='exit'
 alias watchdir='watch -n 5 "ls -lasht"'
 alias idgaf="echo \"¯\_(ツ)_/¯\""
 alias buildin="echo '\nᕕ( ᐛ )ᕗ  buildin...\n'; ./deploy"
@@ -41,6 +51,7 @@ alias mvn='mvn -T 2C'
 
 # use the 'gce' profile, and don't attempt to verify SSL certs
 alias mvn_gce="mvn -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Pgce -T 2C"
+
 # list available rubies
 alias rubies='ruby-build --definitions'
 
