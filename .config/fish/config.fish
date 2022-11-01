f2b18369d33a77ac83da3821a42985e068eb273b
Dylan Carney <dcarney@stripe.com>
2022-11-01 09:55:17 -0700

    [fish] Updates latest fish shell settings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,10 +4,13 @@ function fish_mode_prompt
   # NOOP - Disable vim mode indicator
 end
 
+set GOPATH $HOME/go
+set GOBIN $HOME/go/bin
 set PATH $HOME/bin $PATH
 set PATH /usr/local/opt/go@1.15/bin/ $PATH
+set PATH $HOME/go/bin $PATH
 
 # turns off the GOPACKAGESDRIVER functionality
 # in gocode when using VS Code. This functionality
 # attempts to inform VS Code/gopls about Bazel-managed packages.
-set STRIPE_GOCODE_OPTOUT 1
+set -x STRIPE_GOCODE_OPTOUT 1
